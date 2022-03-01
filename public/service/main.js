ea3079387608d87c8d9d60d6e49d584eca34c2e2
Mateus Malvezzi <mateusmalvezzir@hotmail.com>
2022-03-01 15:14:53 -0300

    feat: creating button and card components

--- public/service/main.js
+++ public/service/main.js
@@ -8,8 +8,9 @@ function get (url) {
 function createCard(product) {
     card = document.createElement('div');
     img = document.createElement('img');
-    pName = document.createElement('p');
+    pName = document.createElement('h3');
     pPrice = document.createElement('p');
+   
 
     img.innerHTML = product.image;
     pName.innerHTML = product.name;
@@ -18,19 +19,40 @@ function createCard(product) {
     card.appendChild(img);
     card.appendChild(pName);
     card.appendChild(pPrice);
+    
 
     return card;
 
+}
+
+function createButton(){
+    container = document.createElement('div')
+    button = document.createElement('button');
+    text = document.createElement('p');
+
+    text.innerHTML = 'COMPRAR';
+
+    container.appendChild(button);
+    button.appendChild(text);
+
+    return container;
 }
 
 function main() {
     let data = get("http://localhost:8888/api/V1/categories/3");
     let product = JSON.parse(data);
     let component = document.getElementById("card-component")
+    let button2 = document.getElementById("button");
+    const sliced = product.items.slice(0, 4);
+      
+
     //Para cada produto --> Criar um card --> Adicionar no component pai
-    product.items.map(element => {
+    sliced.map(element => {
         let card = createCard(element);
         component.appendChild(card);
+        let button = createButton(element);
+        button2.appendChild(button);
+        
     });
 }
 
